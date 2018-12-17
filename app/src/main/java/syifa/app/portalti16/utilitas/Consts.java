78d6714761d54cf6be9ef9b4ac811a068241ecb5
Syifatf <syifatf@gmail.com>
2018-12-17 16:58:39 +0700

    menambahkan consts class pada utilities package

---
+++ app/src/main/java/syifa/app/portalti16/utilitas/Consts.java
@@ -0,0 +1,14 @@
+package syifa.app.portalti16.utilitas;
+
+/**
+ * Created by USER on 17/12/2018.
+ */
+
+public class Consts {
+
+    // membuat
+    public static final String KEY_ACTION_DETAIL = "action";
+
+    public static final String INTENT_ADD = "add";
+    public static final String INTENT_EDIT = "edit";
+}
