56a98a1db8fc3009f34942998e010a5aae9c7cee
Syifatf <syifatf@gmail.com>
2019-01-07 17:20:08 +0700

    tambah mahasiswaDatabase class

--- app/src/main/java/syifa/app/portalti16/data/MahasiswaDatabase.java
+++ app/src/main/java/syifa/app/portalti16/data/MahasiswaDatabase.java
@@ -1,8 +1,15 @@
 package syifa.app.portalti16.data;
 
+import syifa.app.portalti16.entity.Mahasiswa;
+
 /**
  * Created by USER on 07/01/2019.
  */
 
-public class MahasiswaDatabase {
+@Database(entities = {Mahasiswa.class}, vesion = 1, exportSchema)
+public abstract class MahasiswaDatabase {
+    public abstract class Mahasiswa {
+
+    }
+
 }
